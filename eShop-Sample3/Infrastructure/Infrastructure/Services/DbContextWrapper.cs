a7ea578c1e976bee8a821815f6b0cf9a7e2aa22b
MascotYehorKovalov <yehor.kovalov1@nure.ua>
2022-02-16 17:03:43 +0200

    HW3+Sample4

--- eShop-Sample3/Infrastructure/Infrastructure/Services/DbContextWrapper.cs
+++ eShop-Sample3/Infrastructure/Infrastructure/Services/DbContextWrapper.cs
@@ -1,15 +1,17 @@
-using Catalog.Host.Services.Interfaces;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
+using Infrastructure.Services.Interfaces;
 
-namespace Catalog.Host.Services;
+namespace Infrastructure.Services;
 
 public class DbContextWrapper<T> : IDbContextWrapper<T>
     where T : DbContext
 {
     private readonly T _dbContext;
 
-    public DbContextWrapper(IDbContextFactory<T> dbContextFactory) => _dbContext = dbContextFactory.CreateDbContext();
+    public DbContextWrapper(
+        IDbContextFactory<T> dbContextFactory)
+    {
+        _dbContext = dbContextFactory.CreateDbContext();
+    }
 
     public T DbContext => _dbContext;
 
